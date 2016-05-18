f1f11c98534b3a6059fecad4c64f5f44b4af00c7
Tadej Borovšak <tadej.borovsak@xlab.si>
2016-05-18 10:03:27 +0200

    Make deployment ui display single container
    In order to reduce clutter, deployment service now only displays single
    container. In order for this to work, we added another preference to the
    deployment plug-in.
    
    Along the way, separated plug-in completely from DICE Platform, since there is
    no reason for such tight coupling between the two of them. In fact, this
    coupling made adding new preference to plug-in quite painful.
    
    Additionally, with new separation, this plug-in is usable on it's own (without
    having complete DICE Platform installed). This means that it can be used to
    deploy hand-written blueprints directly from Eclipse.

--- org.dice.deployments/src/org/dice/deployments/preferences/DeploymentsPreferenceInitializer.java
+++ org.dice.deployments/src/org/dice/deployments/preferences/DeploymentsPreferenceInitializer.java
@@ -1,13 +1,16 @@
 package org.dice.deployments.preferences;
 
+import org.dice.deployments.DeploymentsActivator;
 import org.dice.deployments.preferences.pages.DeploymentsPreferencesPage;
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
+import org.eclipse.jface.preference.IPreferenceStore;
 
 public class DeploymentsPreferenceInitializer extends AbstractPreferenceInitializer {
 
 	@Override
 	public void initializeDefaultPreferences() {
-		DeploymentsPreferencesPage.getSingleton().initDefaults();
+		IPreferenceStore store = DeploymentsActivator.getDefault().getPreferenceStore();
+		DeploymentsPreferencesPage.initDefaults(store);
 	}
 
 }
--- org.dice.deployments/src/org/dice/deployments/preferences/pages/DeploymentsPreferencesPage.java
+++ org.dice.deployments/src/org/dice/deployments/preferences/pages/DeploymentsPreferencesPage.java
@@ -1,58 +1,55 @@
 package org.dice.deployments.preferences.pages;
 
 import org.dice.deployments.DeploymentsActivator;
-import org.dice.ui.preferences.pages.AbstractOpenBrowserPreferencesPage;
+import org.eclipse.jface.preference.ComboFieldEditor;
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
 import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.preference.IntegerFieldEditor;
+import org.eclipse.jface.preference.StringFieldEditor;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
 
-public class DeploymentsPreferencesPage extends AbstractOpenBrowserPreferencesPage {
+public class DeploymentsPreferencesPage extends FieldEditorPreferencePage
+		implements IWorkbenchPreferencePage {
 
-	private static DeploymentsPreferencesPage singleton;
+	public static final String DESCRIPTION = "Preferences for Deployment Tool";
 
-	public static DeploymentsPreferencesPage getSingleton() {
-		if (singleton == null) {
-			singleton = new DeploymentsPreferencesPage();
-		}
-		return singleton;
+	public static final String PROTOCOL_KEY = "deployments_protocol";
+	public static final String PROTOCOL_VAL = "https";
+	public static final String PROTOCOL_DSC = "Protocol";
+
+	public static final String SERVER_KEY = "deployments_server";
+	public static final String SERVER_VAL = "109.231.122.46";
+	public static final String SERVER_DSC = "Server";
+
+	public static final String PORT_KEY = "deployments_port";
+	public static final int    PORT_VAL = 8000;
+	public static final String PORT_DSC = "Port";
+
+	public static final String CONTAINER_KEY = "deployments_container";
+	public static final String CONTAINER_VAL = "<container-id>";
+	public static final String CONTAINER_DSC = "Container id";
+
+	@Override
+	public void init(IWorkbench workbench) {
+		setPreferenceStore(DeploymentsActivator.getDefault().getPreferenceStore());
+		setDescription(DESCRIPTION);
 	}
 
 	@Override
-	public IPreferenceStore getPluginPreferenceStore() {
-		return DeploymentsActivator.getDefault().getPreferenceStore();
+	protected void createFieldEditors() {
+		String[][] protocols = {{"http", "http"}, {"https", "https"}};
+		addField(new ComboFieldEditor(PROTOCOL_KEY, PROTOCOL_DSC, protocols, getFieldEditorParent()));
+		addField(new StringFieldEditor(SERVER_KEY, SERVER_DSC, getFieldEditorParent()));
+		addField(new IntegerFieldEditor(PORT_KEY, PORT_DSC, getFieldEditorParent()));
+		addField(new StringFieldEditor(CONTAINER_KEY, CONTAINER_DSC, getFieldEditorParent()));
 	}
 
-	@Override
-	protected String getPageDescription() {
-		return "Preferences for Deployment Tool";
-	}
-
-	@Override
-	public String getProtocolIdProperty() {
-		return "deployments_protocol";
-	}
-
-	@Override
-	public String getServerIdProperty() {
-		return "deployments_server";
-	}
-
-	@Override
-	public String getPortIdProperty() {
-		return "deployments_port";
-	}
-
-	@Override
-	protected String getDefaultProtocol() {
-		return PROTOCOL.HTTP.name();
-	}
-
-	@Override
-	protected String getDefaultServer() {
-		return "109.231.122.46";
-	}
-
-	@Override
-	protected int getDefaultPort() {
-		return 8000;
+	public static void initDefaults(IPreferenceStore store) {
+		store.setDefault(PROTOCOL_KEY, PROTOCOL_VAL);
+		store.setDefault(SERVER_KEY, SERVER_VAL);
+		store.setDefault(PORT_KEY, PORT_VAL);
+		store.setDefault(CONTAINER_KEY, CONTAINER_VAL);
 	}
 
 }
